58e4a9134fbdf85aedad59ea7cc035b8c0b6f02f
Keshav sharma <keshav.sharma@novostack.com>
2025-03-27 20:28:09 +0000

    init

---
+++ cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/keshavsharma54126/social/internal/env"
+	"github.com/keshavsharma54126/social/internal/store"
+)
+
+func main() {
+
+	cfg := config{
+		addr: env.GetString("ADDR",":8080"),
+	}
+	store:= store.NewStorage(nil)
+	app := &application{
+		config: cfg,
+		store:store,
+	}
+
+	
+	router:= app.mount()
+
+
+	log.Fatal(app.run(router))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/keshavsharma54126/social
+
+go 1.23.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+)
