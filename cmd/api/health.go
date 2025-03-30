029865390c50f7d2b11994d92b511686d3ad422f
keshav <sharmakeshav54126@gmail.com>
2025-03-31 00:49:01 +0530

    database store repositories for users and posts and their methods

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,6 +3,6 @@ package main
 import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter,r *http.Request) {
-	w.Write([]byte("health is good"))
+  	w.Write([]byte("word is not getting ahy or any in here stronger as we into this is "))
 
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.23.0
 
 require (
 	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/chi/v5 v5.2.1
 )
+
+require github.com/lib/pq v1.10.9 // indirect
