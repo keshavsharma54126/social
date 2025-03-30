435240aaf8bf32f2a85427d0dc73ecbfe726b980
keshav <sharmakeshav54126@gmail.com>
2025-03-31 02:13:49 +0530

    database connection

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -13,10 +13,19 @@ import (
 type application struct {
 	config config
 	store store.Storage
+	
 }
 
 type config struct {
 	addr string
+	db		dbConfig
+}
+
+type dbConfig struct{
+	addr string 
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string 
 }
 
 func (app *application) mount() *chi.Mux{
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/keshavsharma54126/social/internal/db"
 	"github.com/keshavsharma54126/social/internal/env"
 	"github.com/keshavsharma54126/social/internal/store"
 )
@@ -11,8 +12,27 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: env.GetString("DB_ADDR","postgres://user:adminpassword@localhost/social?sslmod=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS",30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS",30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME","15min"),
+		},
 	}
-	store := store.NewStorage(nil)
+
+	db,err := db.New(
+		cfg.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err!=nil{
+		log.Panic(err)
+	}
+
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
