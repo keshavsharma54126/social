200d0e15c654654f55982e6c87089699898b15d5
keshav <sharmakeshav54126@gmail.com>
2025-03-31 04:11:00 +0530

    migrations and makefile

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,24 +13,26 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr: env.GetString("DB_ADDR","postgres://user:adminpassword@localhost/social?sslmod=disable"),
-			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS",30),
-			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS",30),
-			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME","15min"),
+			addr: env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30), // Restore this if needed, was missing in previous state
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
-	db,err := db.New(
-		cfg.addr,
+	db, err := db.New(
+		cfg.db.addr,
 		cfg.db.maxOpenConns,
 		cfg.db.maxIdleConns,
 		cfg.db.maxIdleTime,
 	)
 
-	if err!=nil{
+	if err != nil {
 		log.Panic(err)
 	}
 
+	defer db.Close()
+	log.Print("db connected")
 
 	store := store.NewStorage(db)
 	app := &application{
--- internal/db/db.go
+++ internal/db/db.go
@@ -3,31 +3,48 @@ package db
 import (
 	"context"
 	"database/sql"
+	"strings" // Added import
 	"time"
+
+	_ "github.com/lib/pq" // Import the postgres driver
 )
 
-
-func New(addr string,maxOpenConns,maxIdleConns int,maxIdleTime string)(*sql.DB,error){
-	db,err:= sql.Open("postgres",addr)
-	if err!=nil{
-		return nil,err
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	// Ensure sslmode is disabled, overriding any potential environment issues for diagnosis
+	connStr := addr
+	if !strings.Contains(connStr, "sslmode=") {
+		if strings.Contains(connStr, "?") {
+			connStr += "&sslmode=disable" // Use & if other params exist
+		} else {
+			connStr += "?sslmode=disable"
+		}
 	}
 
-	duration,err:= time.ParseDuration(maxIdleTime)
-
-	if err!=nil{
-		return nil,err
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
 	}
+
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		// It's helpful to close the db if duration parsing fails
+		db.Close()
+		return nil, err
+	}
+
 	db.SetMaxOpenConns(maxOpenConns)
 	db.SetMaxIdleConns(maxIdleConns)
 	db.SetConnMaxIdleTime(duration)
 
-	ctx,cancel:= context.WithTimeout(context.Background(),5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	err = db.PingContext(ctx)
 
-	if err!=nil{
-		return nil,err
+	err = db.PingContext(ctx)
+	if err != nil {
+		// Close the db connection if ping fails
+		db.Close()
+		return nil, err
 	}
-	return db,nil
+
+	return db, nil
 }
